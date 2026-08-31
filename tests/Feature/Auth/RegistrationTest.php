bb7ffef4d4b73b15e17502c76d8c36692e012a60
distributorgame <raincitygamingid@gmail.com>
2026-08-31 23:27:03 +0700

    Add files via upload

---
+++ tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,20 @@
+<?php
+
+test('registration screen can be rendered', function () {
+    $response = $this->get(route('register'));
+
+    $response->assertStatus(200);
+});
+
+test('new users can register', function () {
+    $response = $this->post(route('register.store'), [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'phone' => '081234567890',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(route('after-login', absolute: false));
+});
